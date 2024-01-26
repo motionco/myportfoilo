be6ca82a0b441947515ee6a5cefd733181bcf39c
motionco <motioncomc@gmail.com>
2024-01-26 17:52:43 +0900

    완성

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+export default function Notfound() {
+    return (
+        <Layout>
+            <div>
+                <h2>404</h2>
+                <p>페이지를 찾을 수 없습니다.</p>
+            </div>
+        </Layout>
+    )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatem quae iste minima voluptate tenetur, sunt animi! Error reiciendis quis culpa nostrum voluptatem,
+          accusamus natus, magni expedita enim eaque mollitia!</p>
+      </div>
+    </Layout>
+  )
+}
